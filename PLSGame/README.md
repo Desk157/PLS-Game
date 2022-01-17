17d7354ded9b9f71619a1d41ce3fc3683367c463
Desk157 <97900479+Desk157@users.noreply.github.com>
2022-01-17 15:29:09 +0000

    Delete PLSGame directory

--- PLSGame/README.md
+++
@@ -1,9 +0,0 @@
-# ccagame
-
-## How to run an experiment
-
-
-## Description
-
-cca as a game a bit like https://deepmind.com/research/publications/2020/EigenGame-PCA-as-a-Nash-Equilibrium
-/ https://arxiv.org/pdf/2010.00554.pdf
--- PLSGame/plsgame/datasets/__init__.py
+++
@@ -1,6 +0,0 @@
-from .mnist import mnist_dataset
-from .xrmb import xrmb_dataset
-from .linearspectrum import linear_dataset
-from .exponentialspectrum import exponential_dataset
-from .ukbiobank import ukbb_dataset
-from .utils import get_training_steps
